318b8639a5e402cfd9b0140cc353777d47544a20
Artem Stepanov <temka.do@gmail.com>
2023-04-26 15:09:12 +0300

    Исправил канонический урл в мета-информации

--- settings/global.js
+++ settings/global.js
@@ -2,4 +2,4 @@ export const GLOBAL_SETTINGS = Object.freeze({
   siteName: "Tour Guide Rental System",
 });
 
-export const DOMAIN = "https://tour-guide-gamma.vercel.app";
+export const DOMAIN = "https://montenegro-region.com";
